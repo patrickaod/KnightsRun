22d873d1fe4abd8d9ffa9936c2f505a6050a34f8
Patrick ODoherty <patricius.aod@gmail.com>
2024-05-12 03:46:22 +0100

    Add addenemy method to game constructor

--- assets/js/main.js
+++ assets/js/main.js
@@ -115,5 +115,10 @@ window.addEventListener('load', function () {
             });
             this.UI.draw(context);
         }
+        addEnemy() {
+            if (this.speed > 0 && Math.random() < 0.5) this.enemies.push(new GroundEnemy(this));
+            else if (this.speed > 0) this.enemies.push(new ClimbingEnemy(this));
+            this.enemies.push(new FlyingEnemy(this));
+        }
     }
 })
