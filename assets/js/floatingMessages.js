aecac3190d268b8787a94028689f79101a6fd2c7
Patrick ODoherty <patricius.aod@gmail.com>
2024-05-13 00:43:56 +0100

    Add FloatingMessages constructor, update, draw

---
+++ assets/js/floatingMessages.js
@@ -0,0 +1,24 @@
+export class FloatingMessages {
+    constructor(value, x, y, targetX, targetY){
+        this.value = value;
+        this.x = x;
+        this.y = y;
+        this.targetX = targetX;
+        this.targetY = targetY;
+        this.markForDeletion = false;
+        this.timer = 0;
+    }
+    update(){
+        this.x += (this.targetX - this.x) * 0.03;
+        this.y += (this.targetY - this.y) * 0.03;
+        this.timer++;
+        if (this.timer > 100) this.markForDeletion = true;
+    }
+    draw(context){
+        context.font = '25px Sedan SC';
+        context.fillStyle = 'white';
+        context.fillText(this.value, this.x, this.y);
+        context.fillStyle = 'black';
+        context.fillText(this.value, this.x - 2, this.y - 2);
+    }
+}
