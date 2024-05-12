e8deefdf3076a60d3303c0f4ddb7112bda631214
Patrick ODoherty <patricius.aod@gmail.com>
2024-05-12 03:49:03 +0100

    Add new game, animate func, end and start condition

--- assets/js/main.js
+++ assets/js/main.js
@@ -121,4 +121,19 @@ window.addEventListener('load', function () {
             this.enemies.push(new FlyingEnemy(this));
         }
     }
-})
+        
+    const game = new Game(canvas.width, canvas.height);
+    let lastTime = 0;
+
+
+    function animate(timeStamp) {
+        const deltaTime = timeStamp - lastTime;
+        lastTime = timeStamp;
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        game.update(deltaTime);
+        game.draw(ctx);
+        if (!game.gameOver) requestAnimationFrame(animate);
+    }
+
+    animate(0);
+});
