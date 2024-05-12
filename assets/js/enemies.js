6a054477f42831dced95b19a53c097a612388106
Patrick ODoherty <patricius.aod@gmail.com>
2024-05-13 00:48:09 +0100

    Add enemy constructor, update, draw

---
+++ assets/js/enemies.js
@@ -0,0 +1,28 @@
+class Enemy {
+    constructor(){
+        this.frameX = 0;
+        this.frameY = 0;
+        this.fps = 20;
+        this.frameInterval = 1000/this.fps;
+        this.frameTimer = 0;
+        this.markedForDeletion = false;
+    }
+    update(deltaTime){
+        // movement
+        this.x -= this.speedX + this.game.speed
+        this.y += this.speedY;
+        if (this.frameTimer > this.frameInterval){
+            this.frameTimer = 0;
+            if (this.frameX < this.maxFrame) this.frameX++;
+            else this.frameX = 0;
+        } else {
+            this.frameTimer += deltaTime
+        }
+        // check if off screen
+        if (this.x + this.width < 0) this.markedForDeletion = true;
+    }
+    draw(context){
+        if (this.game.debug) context.strokeRect(this.x, this.y, this.width, this.height);
+        context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+}
