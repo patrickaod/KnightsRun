4d8226b8b246d7fa149f767d5e4dc532f3b89751
patrickaod <patricius.aod@gmail.com>
2024-05-15 06:00:40 +0100

    Update ui win message

--- assets/js/ui.js
+++ assets/js/ui.js
@@ -28,7 +28,7 @@ export class UI {
             context.textAlign = 'center';
             context.font = this.fontSize * 2 + 'px ' + this.fontFamily;
             if (this.game.score > this.game.winningScore){
-                context.fillText('Boo-yah', this.game.width * 0.5, this.game.height * 0.5 - 20);
+                context.fillText('Yay !!', this.game.width * 0.5, this.game.height * 0.5 - 20);
                 context.font = this.fontSize * 0.7 + 'px ' + this.fontFamily;
                 context.fillText('You Win!!!', this.game.width * 0.5, this.game.height * 0.5 + 20);
             } else {
