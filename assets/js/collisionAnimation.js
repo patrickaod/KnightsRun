2811f719d85c5d9c91bdf8870bb8d86955d930c0
patrickaod <patricius.aod@gmail.com>
2024-05-19 17:08:28 +0100

    Add class and method descriptive comments

--- assets/js/collisionAnimation.js
+++ assets/js/collisionAnimation.js
@@ -1,5 +1,7 @@
+// A class representing an animation that occurs when two objects collide.
 export class CollisionAnimation {
     constructor(game, x, y){
+        // Collision Animation Constructor
         this.game = game;
         this.image = document.getElementById('boom');
         this.spriteWidth = 100;
@@ -16,10 +18,12 @@ export class CollisionAnimation {
         this.frameInterval = 1000/this.fps;
         this.frameTimer = 0;
     }
+    // Draws the animation to the specified context.
     draw(context){
         context.drawImage(this.image, this.frameX * this.spriteWidth, 0, this.spriteWidth, 
             this.spriteHeight, this.x, this.y, this.width, this.height);
     }
+    // Updates the animation's position and frame.
     update(deltaTime){
         this.x -= this.game.speed;
         if ( this.frameTimer > this.frameInterval){
