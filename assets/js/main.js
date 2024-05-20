5c3d1ce56e527885d8127ee8cb2375bc489105a1
patrickaod <patricius.aod@gmail.com>
2024-05-20 01:45:50 +0100

    Update tutorial button with modal and off click

--- assets/js/main.js
+++ assets/js/main.js
@@ -171,12 +171,28 @@ window.addEventListener('load', function () {
 });
 
 // Tutorial Button
-document.getElementById("btn").addEventListener("click", function(){
-    alert(
-        " Tutorial: \n" + " Up Arrow / Swipe Up = Jump \n" +
-        " Right & Left Arrow / Swipe Right = Run \n" + " Down Arrow / Down Swipe = Crouch \n" +
-        " Space / Tap = Attack \n" + " Hash / Long Swipe = Dash \n" + " Press r / Swipe Down = Restart \n" + " Press d = Debug"
-    );
+const openButton = document.getElementById("btn");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+
+openButton.addEventListener("click", () => {
+    modal.showModal();
+});
+
+closeButton.addEventListener("click", () => {
+    modal.close();
+});
+
+modal.addEventListener("click", e => {
+    const dialogDimensions = modal.getBoundingClientRect();
+    if ( 
+        e.clientX < dialogDimensions.left ||
+        e.clientX < dialogDimensions.right ||
+        e.clientY < dialogDimensions.top ||
+        e.clientY < dialogDimensions.bottom
+    ) {
+        modal.close();
+    }
 });
 
 // Initialisation or update of Highscore value
